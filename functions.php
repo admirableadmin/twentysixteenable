e0b8a130f292d360fe16fa4ce9b3c5f6ad09b22c
Andreas Peichert <admirableadmin@users.noreply.github.com>
2022-11-04 16:08:28 +0100

    added custom block styling images to frontend and backend

--- functions.php
+++ functions.php
@@ -7,6 +7,14 @@ function theme_enqueue_styles() {
 	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style') );
 }
 
+// child setup theme
+function twentysixteenable_child_setup() {
+	// Add support for editor styles
+	add_theme_support( 'editor-styles' );
+	add_editor_style( 'style.css' );
+}
+add_action( 'after_setup_theme', 'twentysixteenable_child_setup' );
+
 // Remove Google fonts for Twenty Sixteen
 if ( ! function_exists( 'twentysixteen_fonts_url' ) ) :
 function twentysixteen_fonts_url() {
